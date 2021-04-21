71aeb09f870a4ca23f2aaac28e1a5c2c680f8567
Math is My zhyzn <chotchaev.123@live.com>
2021-04-21 13:22:16 +0300

    добавлена функция make_array

--- Практика/Практика.cpp
+++ Практика/Практика.cpp
@@ -1,9 +1,15 @@
 ﻿#include <iostream>
 #include <string>
 #include <clocale>
+#include <cmath>
+#define Rus setlocale(LC_ALL, "Rus")
 
 using namespace std;
 void make_arr(long long key, int arr[]);
+int	number_size(long long key)
+{
+
+}
 string cypher(string sentence, long long key)
 {
 	int arr[100];
@@ -22,4 +28,20 @@ int main()
 	cin >> key;
 	string new_one = cypher(sentence, key);
 	cout << "Зашифрованное предложение: " << new_one;
+	
+
+
+}
+
+void make_arr(long long key, int arr[])
+{
+	int size = number_size(key);
+	int exp = pow(10, size - 1);
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = key / exp;
+		key = key % exp;
+		exp /= 10;
+	}
+
 }

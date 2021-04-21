6941b0ba7d68296151ff18206590cd48eb7f18ba
Math is My zhyzn <chotchaev.123@live.com>
2021-04-21 13:08:16 +0300

    Функции не дописаны. Черновой вариант

--- Практика/Практика.cpp
+++ Практика/Практика.cpp
@@ -1,1 +1,25 @@
-﻿
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+
+using namespace std;
+void make_arr(long long key, int arr[]);
+string cypher(string sentence, long long key)
+{
+	int arr[100];
+	make_arr(key, arr);
+	string new_sentence;
+	return new_sentence;
+}
+
+int main()
+{
+	cout << "Введите предложение: ";
+	string sentence;
+	cin >> sentence;
+	cout << "Введите ключ: ";
+	long long key;
+	cin >> key;
+	string new_one = cypher(sentence, key);
+	cout << "Зашифрованное предложение: " << new_one;
+}
